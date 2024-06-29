d9082f3a57d86e74facaee3408188fa73ce67627
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-06-30 01:19:32 +0530

    styling

--- frontend/src/views/MainComponent.vue
+++ frontend/src/views/MainComponent.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <h1>Online Compiler</h1>
-    <div>
+    <div class="container">
       <label for="language">Select Language:</label>
       <select v-model="selectedLanguage" id="language">
         <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>
       </select>
     </div>
-    <div>
+    <div class="container">
       <label for="code">Code:</label>
       <textarea v-model="code" id="code" rows="10" cols="50"></textarea>
     </div>
-    <button @click="compileCode">Compile</button>
+    <div class="container">
+      <button @click="compileCode">Compile</button>
+    </div>
     <div v-if="output">
       <h2>Output:</h2>
       <pre>{{ output }}</pre>
@@ -20,6 +22,8 @@
 </template>
 
 <script>
+import './MainComponent.css';
+
 export default {
   name: 'MainComponent',
   data() {
@@ -56,33 +60,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-label {
-  display: block;
-  margin-top: 20px;
-}
-
-textarea {
-  width: 100%;
-  margin-top: 10px;
-}
-
-button {
-  margin-top: 20px;
-}
-
-pre {
-  text-align: left;
-  background: #f5f5f5;
-  padding: 10px;
-  border-radius: 5px;
-}
-</style>
